5c4f2fb0c9e4868f6c3382e40aaf2bf65bdb2bc2
Filipe Alves <lf@lcda.com.br>
2020-08-27 18:35:44 -0300

    até a aula 9

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ error: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentsRepository.create(provider, parsedDate);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
